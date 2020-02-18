c36dca970817d48bd145d2739a8424293291949f
Justus Burger <justus@leadhome.co.za>
2020-02-18 09:42:28 +0200

    Add ci.sh

---
+++ .buildkite/ci.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -euo pipefail
+git checkout -B ${BUILDKITE_BRANCH}
+git reset --hard
+
+docker run rm -v "$(pwd):/app" -w /app -e CI="true" -e GH_TOKEN -e NPM_TOKEN node:alpine bash -e "npm ci && npm release"
