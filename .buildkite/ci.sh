75a6be4f997aa39ba73b534a501baed85223a5df
Justus Burger <justus@leadhome.co.za>
2020-02-18 09:49:32 +0200

    Change to docker image with git installed

--- .buildkite/ci.sh
+++ .buildkite/ci.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 git checkout -B ${BUILDKITE_BRANCH}
 git reset --hard
 
-docker run --rm -v "$(pwd):/app" -w /app -e CI="true" -e GH_TOKEN -e NPM_TOKEN node:alpine sh .buildkite/release.sh
+docker run --rm -v "$(pwd):/app" -w /app -e CI="true" -e GH_TOKEN -e NPM_TOKEN timbru31/node-alpine-git:12 sh .buildkite/release.sh
